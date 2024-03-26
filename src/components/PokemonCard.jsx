30befd407756950cb425c0f236ca6d02fdfcde97
Maksim Losjuk <m.lysuk@gmail.com>
2024-03-26 15:30:55 +0100

    pokemon table

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,6 @@
 
+function PokemonCard({ pokemon }) {
 
-
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
 
     return (
         <figure>{pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt= {pokemon.name} />) : (<p>???</p>)}
@@ -12,15 +9,7 @@ function PokemonCard() {
     );
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
+
 
 export default PokemonCard;
